30b30b53b85628cc95585d2ffd44fa847e3cfacc
ghalkidis <georgehalkidis@gmail.com>
2016-05-10 04:11:31 -0400

    Update gui.cpp

--- GUI/gui.cpp
+++ GUI/gui.cpp
@@ -7,12 +7,21 @@
  * License:
  **************************************************************/
 
-#include "FacEMain.h"
+//audio headers
+
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+#include "gui.hpp"
 #include <wx/msgdlg.h>
 
 //(*InternalHeaders(FacEFrame)
 #include <wx/intl.h>
 #include <wx/string.h>
+
 //*)
 
 //helper functions
@@ -51,6 +60,9 @@ const long FacEFrame::ID_BUTTON33 = wxNewId();
 const long FacEFrame::ID_BUTTON111 = wxNewId();
 const long FacEFrame::ID_BUTTON222 = wxNewId();
 const long FacEFrame::ID_BUTTON333 = wxNewId();
+const long FacEFrame::ID_BUTTON1111 = wxNewId();
+const long FacEFrame::ID_BUTTON2222 = wxNewId();
+const long FacEFrame::ID_BUTTON3333 = wxNewId();
 const long FacEFrame::ID_PANEL1 = wxNewId();
 const long FacEFrame::ID_MENUITEM1 = wxNewId();
 const long FacEFrame::idMenuAbout = wxNewId();
@@ -81,6 +93,33 @@ FacEFrame::FacEFrame(wxWindow* parent,wxWindowID id)
     BoxSizer1->Add(Button2, 1, wxALL|wxALIGN_RIGHT|wxALIGN_BOTTOM, 5);
     Button3 = new wxButton(Panel1, ID_BUTTON3, _("Relationships"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON3"));
     BoxSizer1->Add(Button3, 1, wxALL|wxALIGN_BOTTOM|wxALIGN_CENTER_HORIZONTAL, 5);
+    Button11 = new wxButton(Panel1, ID_BUTTON11, _("Happy with Career"), wxPoint(0,0), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON11"));
+    BoxSizer1->Add(Button11, 1, wxALL|wxALIGN_LEFT|wxALIGN_TOP, 5);
+    Button11->Hide();
+    Button22 = new wxButton(Panel1, ID_BUTTON22, _("Happy with Family"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON22"));
+    BoxSizer1->Add(Button22, 1, wxALL|wxALIGN_RIGHT|wxALIGN_TOP, 5);
+    Button22->Hide();
+    Button33 = new wxButton(Panel1, ID_BUTTON33, _("Happy with Relationships"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON33"));
+    BoxSizer1->Add(Button33, 1, wxALL|wxALIGN_TOP|wxALIGN_CENTER_HORIZONTAL, 5);
+    Button33->Hide();
+    Button111 = new wxButton(Panel1, ID_BUTTON111, _("Disgusted with Career"), wxPoint(0,0), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON111"));
+    BoxSizer1->Add(Button111, 1, wxALL|wxALIGN_LEFT|wxALIGN_TOP, 5);
+    Button111->Hide();
+    Button222 = new wxButton(Panel1, ID_BUTTON222, _("Disgusted with Family"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON222"));
+    BoxSizer1->Add(Button222, 1, wxALL|wxALIGN_RIGHT|wxALIGN_TOP, 5);
+    Button222->Hide();
+    Button333 = new wxButton(Panel1, ID_BUTTON333, _("Disgusted with Relationships"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON333"));
+    BoxSizer1->Add(Button333, 1, wxALL|wxALIGN_TOP|wxALIGN_CENTER_HORIZONTAL, 5);
+    Button333->Hide();
+    Button1111 = new wxButton(Panel1, ID_BUTTON1111, _("Surprised with Career"), wxPoint(0,0), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON1111"));
+    BoxSizer1->Add(Button1111, 1, wxALL|wxALIGN_LEFT|wxALIGN_TOP, 5);
+    Button1111->Hide();
+    Button2222 = new wxButton(Panel1, ID_BUTTON2222, _("Suprised with Family"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON2222"));
+    BoxSizer1->Add(Button2222, 1, wxALL|wxALIGN_RIGHT|wxALIGN_TOP, 5);
+    Button2222->Hide();
+    Button3333 = new wxButton(Panel1, ID_BUTTON3333, _("Surprised with Relationships"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON3333"));
+    BoxSizer1->Add(Button3333, 1, wxALL|wxALIGN_TOP|wxALIGN_CENTER_HORIZONTAL, 5);
+    Button3333->Hide();
     Panel1->SetSizer(BoxSizer1);
     BoxSizer1->Fit(Panel1);
     BoxSizer1->SetSizeHints(Panel1);
@@ -101,7 +140,9 @@ FacEFrame::FacEFrame(wxWindow* parent,wxWindowID id)
     StatusBar1->SetStatusStyles(1,__wxStatusBarStyles_1);
     SetStatusBar(StatusBar1);
 
-    Connect(ID_BUTTON1,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&FacEFrame::OnButton1Select);
+    Connect(ID_BUTTON1,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&FacEFrame::OnButtonSelect);
+    Connect(ID_BUTTON2,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&FacEFrame::OnButtonSelect);
+    Connect(ID_BUTTON3,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&FacEFrame::OnButtonSelect);
     Connect(ID_MENUITEM1,wxEVT_COMMAND_MENU_SELECTED,(wxObjectEventFunction)&FacEFrame::OnQuit);
     Connect(idMenuAbout,wxEVT_COMMAND_MENU_SELECTED,(wxObjectEventFunction)&FacEFrame::OnAbout);
     //*)
@@ -120,36 +161,389 @@ void FacEFrame::OnQuit(wxCommandEvent& event)
 
 void FacEFrame::OnAbout(wxCommandEvent& event)
 {
-    wxString msg = wxbuildinfo(long_f);
+    wxString msg = "To navigate through the FacE GUI first signify your emotional sate \nThe three options are happy, disgust, and surprise \nYou then navigate to a subject topic through your eye movements \nThis will output a spoken sentence correlating to your emotional state and the subject chosen";
     wxMessageBox(msg, _("Welcome to FacE"));
 }
 
-void FacEFrame::OnButton1Select(wxCommandEvent& event)
+int FacEFrame::OnButtonSelect(wxCommandEvent& event)
 {
-    for(n=0;n<6;n++){
-        if (mouth=0){//select
-            if (face=0){ //0 is correlating to happy
-                Button1 = new wxButton(Panel1, ID_BUTTON11, _("Happy with current occupation"), wxPoint(0,0), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON11"));
-                Button2 = new wxButton(Panel1, ID_BUTTON22, _("Happy about new job"), wxPoint(0,0), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON22"));
-                Button3 = new wxButton(Panel1, ID_BUTTON33, _("Happy about future career"), wxPoint(0,0), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON33"));
-            }
-            if (face=1){ //1 is disgust
-                Button1 = new wxButton(Panel1, ID_BUTTON111, _(" with current occupation"), wxPoint(0,0), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON111"));
-                Button2 = new wxButton(Panel1, ID_BUTTON222, _(" about new job"), wxPoint(0,0), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON222"));
-                Button3 = new wxButton(Panel1, ID_BUTTON333, _(" about future career"), wxPoint(0,0), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON333"));
-            }
-            if (face=2){ //1 is sad
-                Button1 = new wxButton(Panel1, ID_BUTTON1, _(" with current occupation"), wxPoint(0,0), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON1"));
-                Button2 = new wxButton(Panel1, ID_BUTTON2, _(" about new job"), wxPoint(0,0), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON2"));
-                Button3 = new wxButton(Panel1, ID_BUTTON3, _(" about future career"), wxPoint(0,0), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON3"));
-            }
-            else {
-                std::string str = "Make a selection with facial expression";
-            }
-        if(mouth=1){//move right
+ //Used int face and eye inputs to test button outputs
+ //int face = 0; //0=happy, 1=disgust, 2=relationships
+ //int eye = 2; //0=career, 1=family, 2=relationships
+ int buttonselection;
+        switch(face) {
+            case 0:// happy
+                Button1->Hide();
+                Button2->Hide();
+                Button3->Hide();
+                Button11->Show();
+                Button22->Show();
+                Button33->Show();
+                switch(eye){
+                    case 0:
+                        Button22->Hide();
+                        Button33->Hide();
+                        Button11->Show();
+                        buttonselection=0;
+                    break;
+                    case 1:
+                        Button11->Hide();
+                        Button33->Hide();
+                        Button22->Show();
+                        buttonselection=1;
+                    break;
+                    case 2:
+                        Button11->Hide();
+                        Button22->Hide();
+                        Button33->Show();
+                        buttonselection=2;
+                    break;
+                    default:
+                        std::cout << "Select a button" << endl;
+                    break;
+                }
+        break;
+            case 1: // disgust
+                Button1->Hide();
+                Button2->Hide();
+                Button3->Hide();
+                Button11->Hide();
+                Button22->Hide();
+                Button33->Hide();
+                Button111->Show();
+                Button222->Show();
+                Button333->Show();
+                switch(eye){
+                    case 0:
+                        Button222->Hide();
+                        Button333->Hide();
+                        Button111->Show();
+                        buttonselection=3;
+                    break;
+                    case 1:
+                        Button111->Hide();
+                        Button333->Hide();
+                        Button222->Show();
+                        buttonselection=4;
+                    break;
+                    case 2:
+                        Button111->Hide();
+                        Button222->Hide();
+                        Button333->Show();
+                        buttonselection=5;
+                    break;
+                    default:
+                        std::cout << "Select a button" << endl;
+                    break;
+            break;
+			}
+            case 2: // surprised
+                Button1->Hide();
+                Button2->Hide();
+                Button3->Hide();
+                Button11->Hide();
+                Button22->Hide();
+                Button33->Hide();
+                Button111->Hide();
+                Button222->Hide();
+                Button333->Hide();
+                Button1111->Show();
+                Button2222->Show();
+                Button3333->Show();
+                switch (eye){
+                    case 0:
+                        Button3333->Hide();
+                        Button2222->Hide();
+                        Button1111->Show();
+                        buttonselection=6;
+                    break;
+                    case 1:
+                        Button1111->Hide();
+                        Button3333->Hide();
+                        Button2222->Show();
+                        buttonselection=7;
+                    break;
+                    case 2:
+                        Button1111->Hide();
+                        Button2222->Hide();
+                        Button3333->Show();
+                        buttonselection=8;
+                    break;
+                    default:
+                        std::cout << "Select a button" << endl;
+                        break;
+                }
 
-        }
+            default:
+               std::cout << "Make a selection with facial expression" << endl;
+            break;
 
-        }
-    }
+            }
+  return buttonselection;
+
 }
+
+void playSound(int buttonselection)
+{
+
+    switch(buttonselection){
+	case 0:
+	{
+		// Load a sound buffer from any sound file
+   		sf::SoundBuffer buffer;
+    		if (!buffer.loadFromFile("Sound00.wav")) //in current directory
+        	return;
+
+
+    		// Create a sound instance and play it
+    		sf::Sound sound(buffer);
+    		sound.play();
+
+    		// Loop while the sound is playing
+    		while (sound.getStatus() == sf::Sound::Playing)
+    		{
+        		// how quickly the playing time is updated
+        		sf::sleep(sf::milliseconds(100));
+
+        		// Display playing time <--- not necessary but works better
+			// with this for some reason
+        		std::cout << "\rPlaying... " << sound.getPlayingOffset().asSeconds() << " sec        ";
+        		std::cout << std::flush;
+    		}
+    		std::cout << std::endl << std::endl;
+		playSound();
+		break;
+	}
+	case 1:
+	{	// Load a sound buffer from any sound file
+   	 	sf::SoundBuffer buffer;
+    		if (!buffer.loadFromFile("Sound02.wav")) //in current directory
+        	return;
+
+
+    		// Create a sound instance and play it
+    		sf::Sound sound(buffer);
+    		sound.play();
+
+    		// Loop while the sound is playing
+    		while (sound.getStatus() == sf::Sound::Playing)
+    		{
+        		// how quickly the playing time is updated
+        		sf::sleep(sf::milliseconds(100));
+
+        		// Display playing time <--- not necessary but works better
+			// with this for some reason
+        		std::cout << "\rPlaying... " << sound.getPlayingOffset().asSeconds() << " sec        ";
+        		std::cout << std::flush;
+    		}
+    		std::cout << std::endl << std::endl;
+	playSound();
+	break;
+	}
+
+	case 2:
+	{	// Load a sound buffer from any sound file
+   		sf::SoundBuffer buffer;
+    		if (!buffer.loadFromFile("Sound01.wav")) //in current directory
+        	return;
+
+
+    		// Create a sound instance and play it
+    		sf::Sound sound(buffer);
+    		sound.play();
+
+    		// Loop while the sound is playing
+    		while (sound.getStatus() == sf::Sound::Playing)
+    		{
+        		// how quickly the playing time is updated
+        		sf::sleep(sf::milliseconds(100));
+
+        		// Display playing time <--- not necessary but works better
+			// with this for some reason
+        		std::cout << "\rPlaying... " << sound.getPlayingOffset().asSeconds() << " sec        ";
+        		std::cout << std::flush;
+    		}
+    		std::cout << std::endl << std::endl;
+	playSound();
+	break;
+	}
+
+	case 3:
+	{	// Load a sound buffer from any sound file
+   		sf::SoundBuffer buffer;
+    		if (!buffer.loadFromFile("Sound03.wav")) //in current directory
+        	return;
+
+
+    		// Create a sound instance and play it
+    		sf::Sound sound(buffer);
+    		sound.play();
+
+    		// Loop while the sound is playing
+    		while (sound.getStatus() == sf::Sound::Playing)
+    		{
+        		// how quickly the playing time is updated
+        		sf::sleep(sf::milliseconds(100));
+
+        		// Display playing time <--- not necessary but works better
+			// with this for some reason
+        		std::cout << "\rPlaying... " << sound.getPlayingOffset().asSeconds() << " sec        ";
+        		std::cout << std::flush;
+    		}
+    		std::cout << std::endl << std::endl;
+	playSound();
+	break;
+	}
+
+	case 4:
+	{	// Load a sound buffer from any sound file
+   		sf::SoundBuffer buffer;
+    		if (!buffer.loadFromFile("Sound05.wav")) //in current directory
+        	return;
+
+
+    		// Create a sound instance and play it
+    		sf::Sound sound(buffer);
+    		sound.play();
+
+    		// Loop while the sound is playing
+    		while (sound.getStatus() == sf::Sound::Playing)
+    		{
+        		// how quickly the playing time is updated
+        		sf::sleep(sf::milliseconds(100));
+
+        		// Display playing time <--- not necessary but works better
+			// with this for some reason
+        		std::cout << "\rPlaying... " << sound.getPlayingOffset().asSeconds() << " sec        ";
+        		std::cout << std::flush;
+    		}
+    		std::cout << std::endl << std::endl;
+	playSound();
+	break;
+	}
+
+	case 5:
+	{	// Load a sound buffer from any sound file
+   		sf::SoundBuffer buffer;
+    		if (!buffer.loadFromFile("Sound04.wav")) //in current directory
+        	return;
+
+
+    		// Create a sound instance and play it
+    		sf::Sound sound(buffer);
+    		sound.play();
+
+    		// Loop while the sound is playing
+    		while (sound.getStatus() == sf::Sound::Playing)
+    		{
+        		// how quickly the playing time is updated
+        		sf::sleep(sf::milliseconds(100));
+
+        		// Display playing time <--- not necessary but works better
+			// with this for some reason
+        		std::cout << "\rPlaying... " << sound.getPlayingOffset().asSeconds() << " sec        ";
+        		std::cout << std::flush;
+    		}
+    		std::cout << std::endl << std::endl;
+	playSound();
+	break;
+	}
+	case 6:
+	{	// Load a sound buffer from any sound file
+   		sf::SoundBuffer buffer;
+    		if (!buffer.loadFromFile("Sound06.wav")) //in current directory
+        	return;
+
+
+    		// Create a sound instance and play it
+    		sf::Sound sound(buffer);
+    		sound.play();
+
+    		// Loop while the sound is playing
+    		while (sound.getStatus() == sf::Sound::Playing)
+    		{
+        		// how quickly the playing time is updated
+        		sf::sleep(sf::milliseconds(100));
+
+        		// Display playing time <--- not necessary but works better
+			// with this for some reason
+        		std::cout << "\rPlaying... " << sound.getPlayingOffset().asSeconds() << " sec        ";
+        		std::cout << std::flush;
+    		}
+    		std::cout << std::endl << std::endl;
+	playSound();
+	break;
+	}
+	case 7:
+	{	// Load a sound buffer from any sound file
+   		sf::SoundBuffer buffer;
+    		if (!buffer.loadFromFile("Sound08.wav")) //in current directory
+        	return;
+
+
+    		// Create a sound instance and play it
+    		sf::Sound sound(buffer);
+    		sound.play();
+
+    		// Loop while the sound is playing
+    		while (sound.getStatus() == sf::Sound::Playing)
+    		{
+        		// how quickly the playing time is updated
+        		sf::sleep(sf::milliseconds(100));
+
+        		// Display playing time <--- not necessary but works better
+			// with this for some reason
+        		std::cout << "\rPlaying... " << sound.getPlayingOffset().asSeconds() << " sec        ";
+        		std::cout << std::flush;
+    		}
+    		std::cout << std::endl << std::endl;
+	playSound();
+	break;
+	}
+
+	case 8:
+		// Load a sound buffer from any sound file
+   		sf::SoundBuffer buffer;
+    		if (!buffer.loadFromFile("Sound07.wav")) //in current directory
+        	return;
+
+
+    		// Create a sound instance and play it
+    		sf::Sound sound(buffer);
+    		sound.play();
+
+    		// Loop while the sound is playing
+    		while (sound.getStatus() == sf::Sound::Playing)
+    		{
+        		// how quickly the playing time is updated
+        		sf::sleep(sf::milliseconds(100));
+
+        		// Display playing time <--- not necessary but works better
+			// with this for some reason
+        		std::cout << "\rPlaying... " << sound.getPlayingOffset().asSeconds() << " sec        ";
+        		std::cout << std::flush;
+    		}
+    		std::cout << std::endl << std::endl;
+	playSound();
+	break;
+
+	/*default:
+		std::cout << "something went wrong with choosing correct audio file" << endl;
+            break; */
+	}
+}
+
+/*int main()
+{
+    // Play sound
+    playSound();
+
+    return EXIT_SUCCESS;
+} */
+
+
+
+
+
+        
+    
